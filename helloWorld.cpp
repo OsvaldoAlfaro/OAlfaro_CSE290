c07da640687932b2030cfd7b432d722719c3518f
Osvaldo Alfaro <159959561+OsvaldoAlfaro@users.noreply.github.com>
2026-01-20 13:39:56 -0600

    Create helloWorld.cpp
    Hello World Program- Session #1

---
+++ helloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std:
+
+//function to say hello world
+void welcomeMessage(){
+cout << "Hello World!" << endl;
+}
+
+int main() {
+
+welcomeMessage();
+  
+  return 0;
+}
+
