dfaaa9061fb34c5bb3f5e61a90571288b6e2e651
Osvaldo Alfaro <159959561+OsvaldoAlfaro@users.noreply.github.com>
2026-01-20 16:25:08 -0600

    Refactor main and implement findMax function
    Refactored main function and added findMax function to determine maximum value in the array.

--- session1lab.cpp
+++ session1lab.cpp
@@ -1,38 +1,34 @@
-//importing libraries for select objects
 #include <iostream>
-#include <array>
 
-//declaring namespace to avoid using std:: before every object
 using namespace std;
+
+//function to find the MAX value in the array - Task #3
+int findMax(int arr[], int size) {
+	int maxValue = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (maxValue < arr[i]) {
+			maxValue = arr[i];
+		}
+	}
+	return maxValue;
+}
+
 
 int main() {
 
-	//declaring an array of integers
-	const int SIZE = 5;					//dymanic size declaration
-	array<int, SIZE> values = { 0 };	//array declaration and initialization- Task #1
+	//declaring variables
+	const int size = 6;
+	int maxValue;
+	int array[size];	// declaring array and allocating memory - Task #1
 
-	//obtaining user input assigning values to array - Task #2
-	cout << "---------Enter the " << SIZE << " values you would like to store in the array-------- " << endl;
-	for (int i = 0; i < SIZE; i++) {
-		cout << "Enter value " << (i + 1) << ": ";
-		cin >> values[i];
+	//loop to assign values to this array - Task #2
+	for (int i = 0; i < size; i++) {
+		cout << "Enter your element value for:" << i+1 << "-->";
+		cin >> array[i];
 	}
 
-	//algorithm that finds either the highest or lowest value in the array - Task #3
-	int choice;
-	cout << "Enter 1 to find the highest value or 2 to find the lowest value" << endl;
-	cin >> choice;
-
-	if (choice == 1) {
-
-	}
-	else {
+	//output the highest value 
+	cout << "\nThe max value for this program is: " << (maxValue = findMax(array, size));
 	
-	}
-	
-
-
-
-
 	return 0;
 }
